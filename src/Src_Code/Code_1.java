9a1b501bb9c260ece6e97e9c25c0472a80cc8b8b
ANKITQANEW <qa.ankit91@gmail.com>
2021-10-17 14:51:32 +0530

    first commit ...

--- src/Src_Code/Code_1.java
+++ src/Src_Code/Code_1.java
@@ -3,8 +3,21 @@ package Src_Code;
 public class Code_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		String str = "Hungry";
+        
+		if(str.equalsIgnoreCase("hungry"))
+		{
+			
+			System.out.println("Go for the meal");
+			
+		}
+		
+		else {
+			
+			System.out.println("Do your assignment");
+		    
+		    }
 	}
 
 }
